883d76b1b91513d55be6b0ed0cd442f5c3f28e98
nishchayanand <nishchay.anand63@gmail.com>
2025-09-07 23:32:33 +0530

    implemented hero section of the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { SearchForm } from "@/components/ui/search-form";
 import Image from "next/image";
 
 
@@ -6,7 +7,7 @@ export default function Home() {
     <main>
 
       {/* Hero Section */}
-      <section className="w-full h-screen px-10 pb-10 pt-[4.5rem] flex justify-center items-center">
+      <section className="relative w-full h-screen px-10 pb-20 pt-[4.5rem] flex flex-col items-center">
         <div className="relative w-full h-full">
           <Image
             src="/images/home-page-hero-section.jpg"
@@ -14,19 +15,20 @@ export default function Home() {
             fill={true} // makes the image fill the parent container
             className="object-cover"
           />
+          {/* Search Form */}
+          <div className="absolute w-[calc(100%-2rem)] bottom-[-4rem] left-1/2 transform -translate-x-1/2">
+            <SearchForm />
+          </div>
         </div>
+        
       </section>
-
-      <section className="w-full h-screen bg-amber-400">
-        {/*
-        <Image
-          src="/images/home-page-hero-section.jpg"
-          alt="Hero Image"
-          fill={true} // makes the image fill the parent container
-          className="object-cover object-center"
-        />
-              */}
+      {/*
+      <section className="w-full h-screen flex flex-col pt-10 items-center">
+        <h1 className="text-lg font-bold">HOUSEHOLD OF HOSPITALITY</h1><br />
+        <p className="px-60 text-center">Rooted in the spirit of India’s timeless tradition of “Atithi Devo Bhava”</p><br />
+        <p className="px-96 text-center">Our platform blends modern convenience with heartfelt hospitality. With us, every booking is simple, every stay is seamless, and every guest becomes part of our Gharana.</p>
       </section>
+      */}
 
     </main>
   );
---
+++ src/components/ui/search-form.tsx
@@ -0,0 +1,48 @@
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { CheckInDatePicker } from "@/components/ui/check-in-date-picker";
+import { CheckOutDatePicker } from "@/components/ui/check-out-date-picker";
+
+export function SearchForm() {
+    return (
+    <Card>
+        <CardHeader>
+            <CardTitle>Search Hotels</CardTitle>
+        </CardHeader>
+        <CardContent>
+            <form className="flex justify-center items-center gap-4">
+                
+                {/* Destination */}
+                <div className="flex flex-col gap-2">
+                    <Label htmlFor="destination">Destination</Label>
+                    <Input id="destination" type="text" placeholder="Enter destination" />
+                </div>
+
+                {/* Check-in Date */}
+                <CheckInDatePicker/>
+
+                {/* Check-out Date */}
+                <CheckOutDatePicker />
+                
+                {/* Number of Rooms */}
+                <div className="flex flex-col gap-2">
+                    <Label htmlFor="rooms">Number of Rooms</Label>
+                    <Input id="rooms" type="number" min="1" placeholder="Enter number of rooms" />
+                </div>
+
+                {/* Number of Guests */}
+                <div className="flex flex-col gap-2">
+                    <Label htmlFor="guests">Number of Guests</Label>
+                    <Input id="guests" type="number" min="1" placeholder="Enter number of guests" />
+                </div>
+
+                {/* Search Button */}
+                <Button type="submit" className="h-16 w-48">Search</Button>
+                
+            </form>
+        </CardContent>
+    </Card>
+);
+}
